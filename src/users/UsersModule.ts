c844708c6673ff131783eb8395de6725b9b7e24d
Scorhenao <samcorhenao@gmail.com>
2024-08-14 22:25:35 -0400

    comment UsersModule

--- src/users/UsersModule.ts
+++ src/users/UsersModule.ts
@@ -1,9 +1,14 @@
+// Import the Module decorator from NestJS
 import { Module } from '@nestjs/common';
+// Import the UsersController and UsersService
 import { UsersController } from './UsersController';
 import { UsersService } from './UsersService';
 
+// This line tells NestJS that this class is a module
 @Module({
+  // List the controllers for this module
   controllers: [UsersController],
+  // List the services (providers) for this module
   providers: [UsersService],
 })
 export class UsersModule {}
