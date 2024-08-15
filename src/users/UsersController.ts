fe3a47472728d57b6195e828daf9ae8db49e4f85
Scorhenao <samcorhenao@gmail.com>
2024-08-14 22:25:23 -0400

    comment UsersController

--- src/users/UsersController.ts
+++ src/users/UsersController.ts
@@ -1,4 +1,6 @@
+// Import the Controller decorator from NestJS
 import { Controller } from '@nestjs/common';
 
+// This line tells NestJS that this class is a controller for handling user requests
 @Controller('users')
 export class UsersController {}
