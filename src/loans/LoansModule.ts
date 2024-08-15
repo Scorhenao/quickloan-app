bda771b943e91f027b03d4b12cd44dd54106e4ba
Scorhenao <samcorhenao@gmail.com>
2024-08-14 22:25:16 -0400

    comment LoansModule

--- src/loans/LoansModule.ts
+++ src/loans/LoansModule.ts
@@ -1,8 +1,14 @@
+// Import the Module decorator from NestJS
 import { Module } from '@nestjs/common';
+// Import the LoansController and LoansService
 import { LoansController } from './LoansController';
 import { LoansService } from './LoanService';
+
+// This line tells NestJS that this class is a module
 @Module({
+  // List the controllers for this module
   controllers: [LoansController],
+  // List the services (providers) for this module
   providers: [LoansService],
 })
 export class LoansModule {}
