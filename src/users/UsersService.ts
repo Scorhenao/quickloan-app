6f835b7921cdb2b6f36902f24a1fa5ef878ef99f
Scorhenao <samcorhenao@gmail.com>
2024-08-14 22:25:43 -0400

    comment UsersService

--- src/users/UsersService.ts
+++ src/users/UsersService.ts
@@ -1,4 +1,6 @@
+// Import the Injectable decorator from NestJS
 import { Injectable } from '@nestjs/common';
 
+// This line tells NestJS that this class is a service that can be used in other classes
 @Injectable()
 export class UsersService {}
