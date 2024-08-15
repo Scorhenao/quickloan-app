46d801b0348fce442165ddb4eab31d489fdc0913
Scorhenao <samcorhenao@gmail.com>
2024-08-14 22:24:35 -0400

    comment app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
+// Import the Module decorator from NestJS
 import { Module } from '@nestjs/common';
+// Import the controllers and services for the application
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/UsersController';
 import { UsersService } from './users/UsersService';
 
+// This line tells NestJS that this class is a module
 @Module({
+  // List any other modules that this module depends on (none in this case)
   imports: [],
+  // List the controllers that handle incoming requests
   controllers: [AppController, UsersController],
+  // List the services (providers) that provide functionality
   providers: [AppService, UsersService],
 })
 export class AppModule {}
