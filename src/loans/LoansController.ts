3e209d4e4533c52a374cfd76bba8c3e750b2997b
Scorhenao <samcorhenao@gmail.com>
2024-08-14 22:24:55 -0400

    comment LoansController

--- src/loans/LoansController.ts
+++ src/loans/LoansController.ts
@@ -1,19 +1,26 @@
+// Import necessary parts from NestJS
 import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { LoansService } from './LoanService';
 
+// This line tells NestJS that this class is a controller for handling loan requests
 @Controller('loans')
 export class LoansController {
+  // The constructor gets an instance of LoansService
   constructor(private readonly loansService: LoansService) {}
 
+  // This function handles POST requests to create a new loan
   @Post()
   createLoan(@Body() createLoanDto: any) {
-    // Usar el servicio para calcular el riesgo del préstamo
+    // Use the LoansService to calculate the risk of the loan
     const risk = this.loansService.calculateLoanRisk(createLoanDto);
+    // Return a message with the risk level of the loan
     return `Loan created with ${risk} risk`;
   }
 
+  // This function handles GET requests to get the status of a loan by its ID
   @Get(':id')
   getLoanStatus(@Param('id') id: string) {
+    // Return a message with the status of the loan
     return `Status of loan ${id}`;
   }
 }
